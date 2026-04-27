b40c9e0e1d2c4b165d24427bed48e2843797a316
kosmicplane <aeroberon@gmail.com>
2026-04-26 20:52:36 -0500

    brev update

--- Docker/docker/isaacsim/entrypoint.sh
+++ Docker/docker/isaacsim/entrypoint.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-export ACCEPT_EULA=${ACCEPT_EULA:-Y}
-export PRIVACY_CONSENT=${PRIVACY_CONSENT:-Y}
-export OMNI_KIT_ALLOW_ROOT=${OMNI_KIT_ALLOW_ROOT:-1}
-export NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-all}
-export NVIDIA_DRIVER_CAPABILITIES=${NVIDIA_DRIVER_CAPABILITIES:-all}
-export ROS_DISTRO=${ROS_DISTRO:-humble}
-export ROS_DOMAIN_ID=${ROS_DOMAIN_ID:-42}
+if [ -f /opt/ros/jazzy/setup.bash ]; then
+    source /opt/ros/jazzy/setup.bash
+fi
+
+if [ -f /workspace/ros2_ws/install/setup.bash ]; then
+    source /workspace/ros2_ws/install/setup.bash
+fi
+
+export ACCEPT_EULA=Y
+export PRIVACY_CONSENT=Y
+export OMNI_KIT_ALLOW_ROOT=1
+export NVIDIA_VISIBLE_DEVICES=all
+export NVIDIA_DRIVER_CAPABILITIES=all
+export ROS_DISTRO=jazzy
 export RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION:-rmw_fastrtps_cpp}
-
-if [ -f /opt/ros/humble/setup.bash ]; then
-    source /opt/ros/humble/setup.bash
-fi
-
-if [ -f /workspace/ros2/install/setup.bash ]; then
-    source /workspace/ros2/install/setup.bash
-fi
 
 exec "$@"
